7ba5df14282d74e2e4f0743f8f9e0783ed04bd1b
AirthonValencia <147507918+AirthonValencia@users.noreply.github.com>
2025-07-12 00:08:10 -0300

    Add files via upload

---
+++ optica (2).sql
@@ -0,0 +1,366 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-07-2025 a las 05:07:29
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `optica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `armazon`
+--
+
+CREATE TABLE `armazon` (
+  `Id_Armazon` int(11) NOT NULL,
+  `Material` text NOT NULL,
+  `Color` text NOT NULL,
+  `Tipo` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `Id_Cliente` int(11) NOT NULL,
+  `Nombre_Cliente` text NOT NULL,
+  `Apellido_Cliente` text NOT NULL,
+  `FecNac_Cliente` date NOT NULL,
+  `DNI_Cliente` int(11) NOT NULL,
+  `Telefono_Cliente` bigint(20) NOT NULL,
+  `Email_Cliente` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`Id_Cliente`, `Nombre_Cliente`, `Apellido_Cliente`, `FecNac_Cliente`, `DNI_Cliente`, `Telefono_Cliente`, `Email_Cliente`) VALUES
+(1, 'Franco', 'Acosta', '2006-07-31', 46653723, 3516783467, 'franquitocrack@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallepedido`
+--
+
+CREATE TABLE `detallepedido` (
+  `Id_Detalle` int(11) NOT NULL,
+  `Id_Pedido` int(11) NOT NULL,
+  `Id_Producto` int(11) NOT NULL,
+  `Ojo` text NOT NULL,
+  `Cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_empleado` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefono` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca`
+--
+
+CREATE TABLE `marca` (
+  `Id_Marca` int(11) NOT NULL,
+  `Nombre_Marca` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `Id_Pedido` int(11) NOT NULL,
+  `Fecha_Pedido` date NOT NULL,
+  `Id_Cliente` int(11) NOT NULL,
+  `Estado_Pedido` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `Id_Producto` int(11) NOT NULL,
+  `Tipo_Lente` text NOT NULL,
+  `Material_Base` text NOT NULL,
+  `Id_Marca` int(11) NOT NULL,
+  `Id_Tratamiento` int(11) NOT NULL,
+  `Color` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `receta`
+--
+
+CREATE TABLE `receta` (
+  `id_receta` int(11) NOT NULL,
+  `pedido_id` int(11) NOT NULL,
+  `nombre_cliente` int(11) NOT NULL,
+  `fecha_pedido` date NOT NULL,
+  `od_esfera` decimal(5,0) NOT NULL,
+  `od_eje` int(10) NOT NULL,
+  `od_cilindro` decimal(5,0) NOT NULL,
+  `oi_esfera` decimal(5,0) NOT NULL,
+  `oi_eje` int(10) NOT NULL,
+  `oi_cilindro` decimal(5,0) NOT NULL,
+  `od_adicion` decimal(5,0) NOT NULL,
+  `oi_adicion` decimal(5,0) NOT NULL,
+  `od_prisma` int(11) NOT NULL,
+  `oi_prisma` int(11) NOT NULL,
+  `distancia_pupilar_binocular` varchar(50) NOT NULL,
+  `distancia_pupilar_monocular` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamiento`
+--
+
+CREATE TABLE `tratamiento` (
+  `Id_Tratamiento` int(11) NOT NULL,
+  `Nombre_Tratamiento` text NOT NULL,
+  `Descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `fecha_registro` datetime NOT NULL,
+  `id_empleado` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `usuario`, `contraseña`, `id_cliente`, `fecha_registro`, `id_empleado`) VALUES
+(1, 'lauty123', '123', 0, '2025-05-27 16:45:56', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `armazon`
+--
+ALTER TABLE `armazon`
+  ADD PRIMARY KEY (`Id_Armazon`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`Id_Cliente`);
+
+--
+-- Indices de la tabla `detallepedido`
+--
+ALTER TABLE `detallepedido`
+  ADD PRIMARY KEY (`Id_Detalle`),
+  ADD KEY `Id_Pedido` (`Id_Pedido`),
+  ADD KEY `Id_Producto` (`Id_Producto`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`Id_Marca`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`Id_Pedido`),
+  ADD KEY `Id_Cliente` (`Id_Cliente`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`Id_Producto`),
+  ADD KEY `Id_Marca` (`Id_Marca`),
+  ADD KEY `Id_Tratamiento` (`Id_Tratamiento`);
+
+--
+-- Indices de la tabla `receta`
+--
+ALTER TABLE `receta`
+  ADD PRIMARY KEY (`id_receta`),
+  ADD KEY `pedido_id` (`pedido_id`),
+  ADD KEY `nombre_cliente` (`nombre_cliente`);
+
+--
+-- Indices de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD PRIMARY KEY (`Id_Tratamiento`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `fk_usuario_empleado` (`id_empleado`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `armazon`
+--
+ALTER TABLE `armazon`
+  MODIFY `Id_Armazon` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `Id_Cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `detallepedido`
+--
+ALTER TABLE `detallepedido`
+  MODIFY `Id_Detalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `Id_Marca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `Id_Pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `Id_Producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `receta`
+--
+ALTER TABLE `receta`
+  MODIFY `id_receta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `Id_Tratamiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detallepedido`
+--
+ALTER TABLE `detallepedido`
+  ADD CONSTRAINT `detallepedido_ibfk_1` FOREIGN KEY (`Id_Producto`) REFERENCES `producto` (`Id_Producto`),
+  ADD CONSTRAINT `detallepedido_ibfk_2` FOREIGN KEY (`Id_Pedido`) REFERENCES `pedido` (`Id_Pedido`);
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`Id_Cliente`) REFERENCES `cliente` (`Id_Cliente`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`Id_Marca`) REFERENCES `marca` (`Id_Marca`),
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`Id_Tratamiento`) REFERENCES `tratamiento` (`Id_Tratamiento`);
+
+--
+-- Filtros para la tabla `receta`
+--
+ALTER TABLE `receta`
+  ADD CONSTRAINT `receta_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`Id_Pedido`),
+  ADD CONSTRAINT `receta_ibfk_2` FOREIGN KEY (`nombre_cliente`) REFERENCES `cliente` (`Id_Cliente`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_usuario_empleado` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
